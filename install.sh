32dff9accfcb7f845aaffff058b83268069155bf
exNihlio <clairebea+github@protonmail.com>
2022-01-28 20:18:30 -0700

    Track and install alacritty.yml

--- install.sh
+++ install.sh
@@ -10,11 +10,17 @@ cp .bash_profile ~/
 
 cp .vimrc ~/
 
+cp .tmux.conf ~/
+
 cp .bash_functions ~/
 
 cp .bash_exports ~/
 
 cp .bash_aliases ~/
+
+if [[ -d ~/.config/alacritty ]]; then
+  cp alacritty.yml ~/.config/alacritty/
+fi
 # Only copy the .git if it hasn't be copied yet
 if [[ -z ~/.git ]]; then
   cp -R .git ~/
