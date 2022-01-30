cec69a9e75f8ebd2db56e6ab808764153a2baa2b
exNihlio <clairebea+github@protonmail.com>
2022-01-30 15:38:11 -0700

    Create two separate files for .alacritty, and install based on if system is a Mac or Linux.

--- install.sh
+++ install.sh
@@ -17,10 +17,23 @@ cp .bash_functions ~/
 cp .bash_exports ~/
 
 cp .bash_aliases ~/
-
-if [[ -d ~/.config/alacritty ]]; then
-  cp alacritty.yml ~/.config/alacritty/
+## Install for Mac
+if [[ $(uname)=='Darwin' ]]; then
+  if [[ -d ~/.config/alacritty ]]; then
+    cp .alacritty_macos.yml ~/.config/alacritty/alacritty.yml
+  else
+    mkdir -p ~/.config/.alacritty_macos.yml && cp .alacritty_macos.yml ~/.config/alacritty/alacritty.yml
+  fi
+## Install for Linux
+elif [[ $(uname)=='Linux' ]]; then
+  if [[ -d ~/.config/alacritty ]]; then
+    cp .alacritty_linux.yml ~/.config/alacritty/alacritty.yml
+  else
+    mkdir -p ~/.config/.alacritty_linux.yml && cp .alacritty_linux.yml ~/.config/alacritty/alacritty.yml
+  fi
 fi
+
+
 # Only copy the .git if it hasn't be copied yet
 if [[ -z ~/.git ]]; then
   cp -R .git ~/
